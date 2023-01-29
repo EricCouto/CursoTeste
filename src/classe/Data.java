d36e9d0ea553c51b28ac91ca6a701dae368ca5b8
Eric Couto <ericcouto3@gmail.com>
2023-01-28 21:40:59 -0300

    retirei Scanner de data e usaei contrutores

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -6,7 +6,22 @@ public class Data {
 	String mes;
 	int ano;
 
-//mais flexivel para ser usada em outros contextos
+	Data() {
+		
+			dia = 01;
+			mes = "janeiro";
+			ano = 1970;
+		
+	}
+
+	Data(int diaInit, String mesInit, int anoInit) {
+		dia = diaInit;
+		mes = mesInit;
+		ano = anoInit;
+
+	}
+
+	// mais flexivel para ser usada em outros contextos
 	String organizarData() {
 		return String.format("%d/%s/%d", dia, mes, ano);
 	}
