92a9fadfa412567acb43c0a1da6b39e05fe27d61
Eric Couto <ericcouto3@gmail.com>
2023-02-08 21:13:07 -0300

    utilizando coleções de forma homogenia e ordenada

--- src/colecoes/ConjuntoComportado.java
+++ src/colecoes/ConjuntoComportado.java
@@ -1,10 +1,33 @@
 package colecoes;
 
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class ConjuntoComportado {
 
 	public static void main(String[] args) {
-		
+		// Set<String> listaAprovados = new TreeSet<>();
+		SortedSet<String> listaAprovados = new TreeSet<>();
+		listaAprovados.add("Ana");
+		listaAprovados.add("Carlos");
+		listaAprovados.add("Luca");
+		listaAprovados.add("Pedro");
 
+		for (String candidato : listaAprovados) {
+			System.out.println(candidato);
+
+		}
+		Set<Integer> nums = new TreeSet<>();
+		nums.add(1000);
+		nums.add(1);
+		nums.add(5);
+		nums.add(10);
+		
+		for (int inteiro : nums) {
+			System.out.println(inteiro);
+			
+		}
 	}
 
 }
