3863e5de3c6efe8bd94219d8a9e20efe53b4420e
Eric Couto <ericcouto3@gmail.com>
2023-02-27 23:45:48 -0300

    desafio compra

---
+++ src/oo/composicao/desafio/Item.java
@@ -0,0 +1,11 @@
+package oo.composicao.desafio;
+
+public class Item {
+	int quantidade;
+	Produto produto;
+
+	Item(int quant, Produto produto) {
+		this.quantidade = quant;
+		this.produto = produto;
+	}
+}
