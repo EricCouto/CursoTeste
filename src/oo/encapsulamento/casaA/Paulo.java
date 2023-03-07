c9a10ba2a27897f653fb63211b27d9b35fe85653
Eric Couto <ericcouto3@gmail.com>
2023-03-06 23:21:17 -0300

    demonstração  de encapsulamento utilizando as relações pessoais

---
+++ src/oo/encapsulamento/casaA/Paulo.java
@@ -0,0 +1,15 @@
+package oo.encapsulamento.casaA;
+
+public class Paulo {
+
+	Ana esposa = new Ana();
+
+	void testeAcesso() {
+
+		// System.out.println(esposa.segredo);
+		System.out.println(esposa.facoDentrodeCasa);
+		System.out.println(esposa.formadeFalar);
+		System.out.println(esposa.todosSabem);
+	}
+
+}
