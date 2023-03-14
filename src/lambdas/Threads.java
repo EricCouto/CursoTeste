de436a006a1d2dbbad13babb0f1c4309f94174de
Eric Couto <ericcouto3@gmail.com>
2023-03-14 11:23:14 -0300

    utilizando processamento paralelo com thread e usando lambda e referencia de metodo

---
+++ src/lambdas/Threads.java
@@ -0,0 +1,61 @@
+package lambdas;
+
+public class Threads {
+
+	public static void main(String[] args) {
+
+		Runnable trabalho1 = new Trabalho1();
+		Runnable trabalho2 = new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					System.out.println("tarefa #02 !!");
+					try {
+						Thread.sleep(100);
+					} catch (Exception e) {
+
+					}
+
+				}
+			}
+		};
+
+		Runnable trabalho3 = () -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("tarefa #03 !!!");
+				try {
+					Thread.sleep(100);
+				} catch (Exception e) {
+
+				}
+
+			}
+		};
+
+		Runnable trabalho4 = Threads::trabalho4;
+
+		Thread t1 = new Thread(trabalho1);
+		Thread t2 = new Thread(trabalho2);
+		Thread t3 = new Thread(trabalho3);
+		Thread t4 = new Thread(trabalho4);
+
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
+
+	static void trabalho4() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("tarefa #04 !!!!");
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+
+			}
+
+		}
+	}
+
+}
