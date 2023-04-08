f8b755dc95892664854b9e21dad33b2e2fd36ef7
Eric Couto <ericcouto3@gmail.com>
2023-04-07 21:27:00 -0300

    usando chave e valor com generics

---
+++ src/generics/ParesTeste.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class ParesTeste {
+
+	public static void main(String[] args) {
+		Pares<Integer, String> resultadoConcurso = new Pares<>();
+
+		resultadoConcurso.adicionar(1, "Maria");
+		resultadoConcurso.adicionar(2, "ana");
+		resultadoConcurso.adicionar(3, "gui");
+		resultadoConcurso.adicionar(4, "eric");
+		resultadoConcurso.adicionar(2, "lucas");
+		
+		System.out.println(resultadoConcurso.getValor(2));
+	}
+
+}
