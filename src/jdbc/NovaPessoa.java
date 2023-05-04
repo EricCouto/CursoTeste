9dd94902e3f0d67d0bda0de1e79fde0c9c6addbf
Eric Couto <ericcouto3@gmail.com>
2023-05-04 00:07:25 -0300

    adicionando pessoa ao Banco

---
+++ src/jdbc/NovaPessoa.java
@@ -0,0 +1,28 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class NovaPessoa {
+	public static void main(String[] args) throws SQLException {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("informe o nome: ");
+		String nome = entrada.next();
+
+		Connection conexao = FabricaConexao.getConnection();
+		
+		String sql = "INSERT INTO pessoas (nome) VALUES (?)";
+		//String sql = "INSERT INTO pessoas (nome, codigo) VALUES (?, ?)";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, nome);
+		//stmt.setInt(2, 1);
+		stmt.execute();
+
+		System.out.println("pessoa incluida com sucesso");
+		entrada.close();
+	}
+}
